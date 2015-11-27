243ec1a6374d83092eea47c48c645e9063c42e1b
kotajima <ykotajima1031@gmail.com>
2015-11-27 19:25:23 +0900

    ログイン画面追加等
    ログイン画面、アカウント登録画面の実装
    他微調整

---
+++ Calendar_html/login.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$account = filter_input(INPUT_POST, "account");
+$pass = filter_input(INPUT_POST, "password");
+$success = true;
+
+if ($success) {
+    echo 'success';
+} else {
+    echo 'error';
+}
+?>
